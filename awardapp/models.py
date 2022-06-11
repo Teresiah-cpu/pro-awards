85775ccc154d66c35d112b2b5071d97aeeccb556
Teresiah Githua <terrywambo2018@gmail.com>
2022-06-11 19:30:45 +0300

    ft:this will create a profile model

--- awardapp/models.py
+++ awardapp/models.py
@@ -1,3 +1,26 @@
+# from django.db import models
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from phone_field import PhoneField
+from cloudinary.models import CloudinaryField
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,related_name='profile')
+    bio = models.TextField(max_length=400, blank=True)
+    name = models.CharField(blank=True, max_length=120)
+    profile_pic = CloudinaryField('image', default='https://res.cloudinary.com/samm-gallery/image/upload/v1649751098/Ninja_fjh4xm.png')
+    phone_number = PhoneField(max_length=15, blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
