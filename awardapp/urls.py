9e3fa8e61125065ff26117a1ac281618790d4abb
Teresiah Githua <terrywambo2018@gmail.com>
2022-06-13 22:17:59 +0300

    ft:this will set the url path for review

--- awardapp/urls.py
+++ awardapp/urls.py
@@ -1,7 +1,19 @@
-# from django.conf.urls import re_path
-from django.urls import path,include
+# from django.conf.urls import path as url
+from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
-    path('',views.index, name='home'),
+    path('',views.home,name = 'home'),
+    path('profile/user', views.profile, name='profile'),
+    path('project/new', views.new_project, name='new_project'),
+    path('project/(\d+)', views.review, name='review'),
+    path('api/profile/', views.profileList.as_view()),
+    path('api/profile/profile-id/(<pk>[0-9]+)/',views.ProfileDescription.as_view()),
+    path('api/project/', views.projectList.as_view()),
+    path('api/project/project-id/(<pk>[0-9]+)/',views.ProjectDescription.as_view()),
+    path('search/', views.search_project, name='search_project')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
